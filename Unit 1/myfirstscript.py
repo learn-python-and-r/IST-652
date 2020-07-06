56f73a710a6ec33edcf13e3f47e04f061ea50c72
learn-python-and-r <63880023+learn-python-and-r@users.noreply.github.com>
2020-07-06 17:50:13 -0400

    Add files via upload

---
+++ Unit 1/myfirstscript.py
@@ -0,0 +1,12 @@
+import re
+
+#print('Hello World')
+
+fruits = ['apple','pear','orange']
+
+# Write a loop that prints the name of each fruit
+
+for fruit in fruits:
+    print(fruit)
+    
+# This is a simple script
